3656d0498e4a0d2f3e44172ece2a68a35e4064f2
preeti <118329523+lalpreeti@users.noreply.github.com>
2023-04-28 21:22:06 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,4 @@
 //your JS code here. If required.
-//your JS code here. If required.
 const images = [
   {
     url: "https://picsum.photos/id/237/200/300",
@@ -48,11 +47,3 @@ const button = document.getElementById('download-images-button');
 button.addEventListener('click', () => {
   downloadImages(images);
 });
-const output = document.getElementById("output");
-const btn = document.getElementById("download-images-button");
-
-const images = [
-  { url: "https://picsum.photos/id/237/200/300" },
-  { url: "https://picsum.photos/id/238/200/300" },
-  { url: "https://picsum.photos/id/239/200/300" },
-];
